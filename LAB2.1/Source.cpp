9cac691e77cefca194e92aad5c9b5dc9bb8bad95
Denys Kachalup <denys-maksym.kachalup.itis.2020@lpnu.ua>
2020-10-10 12:45:55 +0300

    first branch_02 commit

--- LAB2.1/Source.cpp
+++ LAB2.1/Source.cpp
@@ -18,8 +18,8 @@ int main()
 	z1 = (1 - (2 * (sin(a) * sin(a)))) / (1 + (2 * sin(a) * cos(a)));
 	z2 = (1 - tan(a)) / (1 + tan(a));
 
-	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	//cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
